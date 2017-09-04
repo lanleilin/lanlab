9bc31481f9959ba08c378a9183c638f90b31467b
lanleilin <2476081528@qq.com>
2017-09-05 01:05:38 +0800

    iterate method

--- mapTest.js
+++ mapTest.js
@@ -1,13 +1,18 @@
 "use strict";
 
 window.onload = function () {
-    function pow(x) {
-        return x * x;
-    }
-    var arr = [1, 3, 6, 7, 9, 2];
-    console.log(arr.map(pow));
-    var a = 3;
-    var b = 3;
+	//Map是一组键值对的结构，具有极快的查找速度
+//  function pow(x) {
+//      return x * x;
+//  }
+//  var arr = [1, 3, 6, 7, 9, 2];
+//  console.log(arr.map(pow));
+//  var a = 3;
+//  var b = 3;
+    
+//  var m = new Map([['Michael', 95], ['Bob', 75], ['Tracy', 85]]);
+//	console.log(m.get('Michael')) // 95
+//  console.log('success')
 };
 
 //# sourceMappingURL=mapTest.js.map
