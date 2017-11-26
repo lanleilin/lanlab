5c6b922cc5b365f5469632d1c91c2d23bed63b08
lanleilin <2476081528@qq.com>
2017-11-26 23:07:54 +0800

    read
    Change-Id: I5f1b7bbae2fc851bae5f26d01e55cb1eb1856d4e

--- js/lanll.js
+++ js/lanll.js
@@ -78,3 +78,50 @@ function urlToObj(url) {
     }
     return js;
 }
+
+// 获取时间
+var myDate = new Date();
+myDate.getYear(); //获取当前年份(2位)
+myDate.getFullYear(); //获取完整的年份(4位,1970-????)
+myDate.getMonth(); //获取当前月份(0-11,0代表1月)
+myDate.getDate(); //获取当前日(1-31)
+myDate.getDay(); //获取当前星期X(0-6,0代表星期天)
+myDate.getTime(); //获取当前时间(从1970.1.1开始的毫秒数)
+myDate.getHours(); //获取当前小时数(0-23)
+myDate.getMinutes(); //获取当前分钟数(0-59)
+myDate.getSeconds(); //获取当前秒数(0-59)
+myDate.getMilliseconds(); //获取当前毫秒数(0-999)
+myDate.toLocaleDateString(); //获取当前日期
+var mytime = myDate.toLocaleTimeString(); //获取当前时间
+myDate.toLocaleString();
+
+
+// var num=1250;
+// var mon=8000;
+// for(var i=1;i<100;i++){
+// 	console.log('today is  '+i)
+// 	console.log(num-=40)
+// 	if(i%31==0){
+// 		console.log('++++++++++++++++++++++++')
+// 		mon-=2000
+// 		console.log("still  "+ mon)
+// 		num+=1400
+// 	}
+// }
+
+function getTime() {
+    var myDate = new Date();
+    return myDate.toLocaleTimeString();
+}
+console.log('时间：  ' + getTime())
+
+function calc(cb, cb1, cb2) {
+    (function() {
+        cb();
+    })();
+}
+
+function log() {
+    console.log('str');
+}
+// calc(log);
