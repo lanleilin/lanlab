bce2b8eae212bf7a180ffcc6f6c95ffb65d8400e
lanleilin <2476081528@qq.com>
2018-01-16 16:48:48 +0800

    map
    Change-Id: I0add961bd924ddb4738033c15ae85c1ef8e2dfc7

--- learnJS/samples/startserver/readdir.js
+++ learnJS/samples/startserver/readdir.js
@@ -1,32 +1,29 @@
-let  fs = require('fs');
-let  join = require('path').join;
-var path = require('path');
+const  fs = require('fs');
+const  join = require('path').join;
 /**
  * 
  * @param startPath  起始目录文件夹路径
  * @returns {Array}
  */
-    var filePath = path.resolve(__dirname + pathName);
-fs.stat(filePath,(err,stats)=>{
-	if(!err && stats.isDirectory()){
-		console.log(stats)
-	}
-})
 function findSync(startPath) {
     let result=[];
     function finder(path) {
         let files=fs.readdirSync(path);
-        // console.log(files)
         files.forEach((val,index) => {
             let fPath=join(path,val);
             let stats=fs.statSync(fPath);
-            if(stats.isDirectory()) finder(fPath);
-            if(stats.isFile()) result.push(fPath);
+            if(stats.isDirectory()) {
+            	result.push(fPath);
+            	// 递归读取文件夹下文件
+            	// finder(fPath)
+            };
+            // 读取文件名
+            // if(stats.isFile()) result.push(fPath);
         });
 
     }
     finder(startPath);
-    // console.log(result)
+    console.log(result)
     return result;
 }
 let fileNames=findSync('./');
---
+++ learnJS/samples/startserver/readdirname.js
@@ -0,0 +1,14 @@
+module.exports = function(...rest) {
+    var sum = 0;
+    for (let n of rest) {
+        sum += n;
+    }
+    return sum;
+};
+// module.exports = function(...rest) {
+//     var sum = 0;
+//     for (let n of rest) {
+//         sum += n;
+//     }
+//     return sum;
+// }
